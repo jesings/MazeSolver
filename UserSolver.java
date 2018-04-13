1d00b77566df81a94800af7979cfc44966e327ac
Yoshiya Ohno <yohno@stuy.edu>
2018-04-12 21:52:25 -0400

    Update MazeSolver with new Vector functionality

--- UserSolver.java
+++ UserSolver.java
@@ -9,9 +9,9 @@ its worth the practice anyway
 */
 
 public abstract class UserSolver{
-	//uhh
-	public static void main(String[] args) throws java.io.FileNotFoundException {
-		MazeSolver.solve( new Maze("TestMaze.txt", 0, 0) );
-	}
-	//cool
+    //uhh
+    public static void main(String[] args) throws java.io.FileNotFoundException {
+        MazeSolver.solve( new Maze("mazes/testMaze.txt", 0, 0) );
+    }
+    //cool
 }
